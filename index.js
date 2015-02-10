fa83c35a86a950228edac4e44005c1a19cf9701f
Antonio Brandao <antoniobrandaodesign@gmail.com>
2015-02-10 00:23:45 +0000

    Added source

--- index.js
+++ index.js
@@ -1,3 +1,313 @@
-'use strict';
+// 'use strict';
 
-module.exports = function() {};
+var modal_base_element;
+var modal_background;
+var modal_element;
+var text_element_title;
+var text_element_text;
+var buttons_container;
+var button_confirm;
+var button_cancel;
+var button_confirm_p;
+var button_cancel_p;
+
+var modal_width 		= 440;
+var modal_height 		= 200;
+var modal_width_px 		= String(modal_width) + 'px';
+var modal_height_px		= String(modal_height) + 'px';
+
+var settings = {
+
+	closeOnOKClick 			: false,
+	type 					: 'simple',
+	titleText				: 'Title',
+	textText				: 'Text',
+	confirmButtonText 		: 'OK',
+	cancelButtonText 		: 'Cancel',
+	showTitle 				: true,
+	showText 				: true,
+	fontFamily 				: 'sans serif',
+	titleMarginBottom 		: '30px',
+	buttonsMarginTop 		: '30px',
+	addButtons 				: false,
+	showConfirmButton 		: true,
+	showCancelButton 		: true,
+	confirmButtonColor 		: '#27AE60',
+	cancelButtonColor 		: '#E74C3C',
+	confirmButtonTextColor	: 'white',
+	cancelButtonTextColor	: 'white',
+	defaultCancelIsClose	: true,
+	defaultOKIsClose		: false,
+	cancelCallback			: null,
+	confirmCallback			: null,
+	fadeOut					: true,
+	modalPadding			: '30px 70px 30px 70px',
+};
+
+
+module.exports = {
+
+    createModal: function(options)
+    {
+    	console.log('createModal');
+
+    	$.extend( settings, options );
+
+    	var w=window,
+		d=document,
+		e=d.documentElement,
+		g=d.getElementsByTagName('body')[0],
+		x=w.innerWidth||e.clientWidth||g.clientWidth,
+		y=w.innerHeight||e.clientHeight||g.clientHeight;
+
+		var window_width 		= x;
+		var window_height 		= y;
+
+		var window_width_px 	= String(window_width) + 'px';
+		var window_height_px	= String(window_height) + 'px';
+
+    	var title 				= 'title';
+    	var text 				= 'text';
+
+        modal_base_element		= document.createElement('DIV');
+        modal_background		= document.createElement('DIV');
+        modal_element 			= document.createElement('DIV');
+        text_element_title 		= document.createElement('H3');
+        text_element_text 		= document.createElement('p');
+
+        modal_background.addClass('ui-modal-background');
+        modal_element.addClass('ui-modal-element');
+
+        if (settings.showConfirmButton || settings.showCancelButton)
+        {
+        	console.log('1');
+        	settings.addButtons 						= true;
+        	buttons_container							= document.createElement('DIV');
+        	buttons_container.style.marginTop 			= settings.buttonsMarginTop;
+        	buttons_container.setAttribute('id', 		'ui-modal-buttons');
+
+        	if (settings.showCancelButton)
+        	{
+        		console.log('2');
+	        	button_cancel							= document.createElement('DIV');
+	        	button_cancel.addClass('ui-button');
+	        	button_cancel.addClass('cancel');
+	        	button_cancel.style.backgroundColor 	= settings.cancelButtonColor;
+	        	button_cancel.style.color 				= settings.cancelButtonTextColor;
+	        	button_cancel.style.paddingTop 			= '10px';
+	        	button_cancel.style.paddingBottom 		= '10px';
+	        	button_cancel.style.paddingLeft 		= '20px';
+	        	button_cancel.style.paddingRight 		= '20px';
+	        	button_cancel.style.minWidth 			= '80px';
+	        	if (settings.showConfirmButton ) 		{ button_cancel.style.marginRight = '40px'; };
+	        	button_cancel.textContent 				= settings.cancelButtonText;
+	        	// button_cancel.appendChild(button_cancel_p);
+	        	buttons_container.appendChild(button_cancel);
+
+	        	if (settings.defaultCancelIsClose) 
+	        	{ 		 button_cancel.addEventListener('click', closeModal);
+	        	} else { button_cancel.addEventListener('click', settings.cancelCallback); }
+        	};
+
+        	if (settings.showConfirmButton)
+        	{
+        		console.log('3');
+	        	button_confirm							= document.createElement('DIV');
+	        	button_confirm.addClass('ui-button');
+	        	button_confirm.addClass('confirm');
+	        	button_confirm.style.backgroundColor	=  settings.confirmButtonColor;
+	        	button_confirm.style.color 				= settings.confirmButtonTextColor;
+	        	button_confirm.style.paddingTop 		= '10px';
+	        	button_confirm.style.paddingBottom 		= '10px';
+	        	button_confirm.style.paddingLeft 		= '20px';
+	        	button_confirm.style.paddingRight 		= '20px';
+	        	button_confirm.style.minWidth 			= '80px';
+	        	button_confirm.textContent 				= settings.confirmButtonText;
+	        	// button_confirm.appendChild(button_confirm_p);
+	        	buttons_container.appendChild(button_confirm);
+	        	console.log('settings.defaultOKIsClose: ' + settings.defaultOKIsClose);
+	        	if (settings.defaultOKIsClose) 
+	        	{ 		 
+	        		button_confirm.addEventListener('click', closeModal);
+	        	} 
+	        	else
+	        	{ 
+	        		button_confirm.addEventListener('click', handleConfirmCallback);
+	        	}
+        	};
+        }
+        else
+        {
+        	settings.addButtons = false;
+        }
+        
+        if (settings.showTitle) 
+        {
+        	text_element_title.textContent 			= settings.titleText;
+        	text_element_title.style.marginBottom 	= settings.titleMarginBottom;
+        	text_element_title.style.fontFamily 	= settings.fontFamily;
+        };
+        
+        if (settings.showText) 
+        {
+        	text_element_text.textContent 			= settings.textText;
+        	text_element_text.style.fontFamily 		= settings.fontFamily;
+        };
+
+		modal_base_element.style.webkitTransition 	= 'all 0.5s';
+		modal_base_element.style.mozTransition 		= 'all 0.5s';
+		modal_base_element.style.msTransition 		= 'all 0.5s';
+		modal_base_element.style.oTransition 		= 'all 0.5s';
+
+		modal_base_element.style.position			= 'fixed';
+		modal_base_element.style.top				= '0';
+		modal_base_element.style.left				= '0';
+		modal_base_element.style.width				= window_width_px;
+		modal_base_element.style.height				= window_height_px;
+		modal_base_element.style.zIndex				= '1000';
+		modal_base_element.style.opacity			= '1';
+
+    	
+
+		modal_background.style.position				= 'fixed';
+		modal_background.style.top					= '0';
+		modal_background.style.left					= '0';
+		modal_background.style.width				= window_width_px;
+		modal_background.style.height				= window_height_px;
+		modal_background.style.backgroundColor 		= 'black';
+		modal_background.style.display				= 'block';
+		modal_background.style.opacity 				= '0.3';
+
+		modal_element.style.position				= 'fixed';
+        // modal_element.style.width					= modal_width_px;
+        // modal_element.style.height					= modal_height_px;
+        modal_element.style.backgroundColor 		= 'white';
+		modal_element.style.display					= 'block';
+		modal_element.style.textAlign				= 'center';
+        modal_element.style.borderRadius			= '3px';
+		modal_element.style.boxSizing				= 'border-box';
+        // modal_element.style.padding 				= '20px';
+		modal_element.style.padding 				= settings.modalPadding;
+
+		modal_element.style.top						= String((window_height / 2) - (modal_height / 2)) + 'px';
+		modal_element.style.left					= String((window_width / 2) - (modal_width / 2)) + 'px';
+
+		modal_base_element.appendChild(modal_background);
+		modal_base_element.appendChild(modal_element);
+		
+        if (settings.showTitle) {
+        	modal_element.appendChild(text_element_title);
+        };
+        
+        if (settings.showText) {
+        	modal_element.appendChild(text_element_text);
+        };
+
+		if (settings.addButtons) {
+			modal_element.appendChild(buttons_container);
+		};
+
+		modal_base_element.setAttribute('id', 'ui-modal');
+		modal_element.setAttribute('id', 'ui-modal-element');
+
+		document.getElementById('root-wrapper').appendChild(modal_base_element);
+
+		if (settings.closeOnBGClick) {
+			modal_background.addEventListener('click', closeModal);
+		};
+
+		addViewportListeners();
+		adjustViewPortModal();
+    }
+}
+
+var closeModal = function()
+{
+	removeViewportListeners();
+	
+	if (settings.showCancelButton) 
+	{
+		if (settings.defaultCancelIsClose) 
+		{
+			button_cancel.removeEventListener('click', closeModal);
+		}
+		else
+		{
+			button_cancel.removeEventListener('click', settings.cancelCallback); 
+		}
+	};
+
+	if (settings.showConfirmButton) 
+	{
+		if (settings.defaultOKIsClose) 
+		{
+			button_confirm.removeEventListener('click', closeModal);
+		} 
+		else
+		{ 
+			button_confirm.removeEventListener('click', settings.confirmCallback);	
+		}
+	};
+
+	var removeElement = function() {
+		document.getElementById('root-wrapper').removeChild(document.getElementById('ui-modal'));
+	}
+
+	if (settings.fadeOut) 
+	{
+		modal_base_element.style.opacity = '0';
+		
+		setTimeout(function() { removeElement(); }, 500)
+	}
+	else { removeElement(); }
+}
+
+var addViewportListeners = function()
+{
+	console.log('addViewportListeners');
+	if(window.attachEvent) 			 	{ console.log('1'); window.attachEvent('onresize', 	adjustViewPortModal);
+	} else if(window.addEventListener) 	{ console.log('3'); window.addEventListener('resize', adjustViewPortModal, true);
+	} else { 							//The browser does not support Javascript event binding
+	}
+}
+
+var removeViewportListeners = function()
+{
+	console.log('removeViewportListeners');
+	if(window.detachEvent)  				{ console.log('2'); window.detachEvent('onresize', 		adjustViewPortModal);
+	} else if(window.removeEventListener) 	{ window.removeEventListener('resize', 	adjustViewPortModal);
+	} else { 								//The browser does not support Javascript event binding
+	}
+}
+
+var handleConfirmCallback = function()
+{
+	settings.confirmCallback();
+	
+	closeModal();
+}
+
+var adjustViewPortModal = function()
+{
+	console.log('addViewportListeners');
+
+    var w=window,
+		d=document,
+		e=d.documentElement,
+		g=d.getElementsByTagName('body')[0],
+		x=w.innerWidth||e.clientWidth||g.clientWidth,
+		y=w.innerHeight||e.clientHeight||g.clientHeight;
+
+	var modal_instance 				= document.getElementById('ui-modal-element');
+	
+	if (modal_instance) {
+		console.log('modal_instance.offsetWidth: ' + modal_instance.offsetWidth);
+		console.log('modal_instance.offsetHeight: ' + modal_instance.offsetHeight);
+
+		modal_background.style.width	= String(x) + 'px';
+		modal_background.style.height	= String(y) + 'px';
+
+		modal_instance.style.left		= String((x / 2) - (modal_instance.offsetWidth / 2)) + 'px';
+		modal_instance.style.top		= String((y / 2) - (modal_instance.offsetHeight / 2)) + 'px';
+	};
+}
